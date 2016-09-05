d5d9477a880d12464764bfda61f63eedc4779960
nk <nkolesnikov@mail.ru>
2016-09-05 22:34:26 +0300

    IntroCPP_1_7_Task7

--- 1_7_For_Arrays/Task5/main.cpp
+++ 1_7_For_Arrays/Task5/main.cpp
@@ -1,10 +1,42 @@
+/*
+ Найдите количество положительных элементов в данном массиве.
+ Формат входных данных
+ В первой строке вводится количество элементов в массиве. Во второй строке вводятся элементы массива.
+ Формат выходных данных
+ Выведите ответ на задачу.
+ Sample Input:
+ 5
+ 1 -2 3 -4 5
+ Sample Output:
+ 3
+ */
+
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main() {
   
-  cout << "Hello, World!\n";
+  int n;
+  cin >> n;
+  vector <int> a;
+  //считывание
+  for (int i = 0; i < n; i++) {
+    int temp;
+    cin >> temp;
+    a.push_back(temp);
+  }
+  
+  int count = 0;
+  
+  for (int i = 0; i < n; i++) {
+    if (a[i] > 0) {
+      count++;
+    }
+  }
+  
+  cout << count;
   
   return 0;
 }
--- 1_7_For_Arrays/Task6/main.cpp
+++ 1_7_For_Arrays/Task6/main.cpp
@@ -1,10 +1,33 @@
-#include <iostream>
-
-using namespace std;
+/*
+ Дан массив чисел. Выведите все элементы массива, которые больше предыдущего элемента .
+ Формат входных данных
+ В первой строке вводится количество элементов в массиве. Во второй строке вводятся элементы массива.
+ Формат выходных данных
+ Выведите ответ на задачу.
+ Sample Input:
+ 5
+ 1 5 2 4 3
+ Sample Output:
+ 5 4
+ */
 
 int main() {
   
-  cout << "Hello, World!\n";
+  int n;
+  cin >> n;
+  vector <int> a;
+  //считывание
+  for (int i = 0; i < n; i++) {
+    int temp;
+    cin >> temp;
+    a.push_back(temp);
+  }
+  
+  for (int i = 1; i < n; i++) {
+    if (a[i] > a[i-1]) {
+      cout << a[i] << " ";
+    }
+  }
   
   return 0;
 }
