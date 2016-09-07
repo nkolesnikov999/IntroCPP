df420828e22f55fb62814cf23fc847d058dfaeda
nk <nkolesnikov@mail.ru>
2016-09-07 15:09:57 +0300

    IntroCPP_1_9_Task10

--- 1_9_Functions/Task10/main.cpp
+++ 1_9_Functions/Task10/main.cpp
@@ -1,10 +1,44 @@
+/*
+ Дано натуральное число n > 1. Выведите его наименьший делитель, отличный от 1.
+ Решение оформите в виде функции MinDivisor(n). Количество операций в программе должно быть пропорционально корню из n.
+ Указание. Если у числа n нет делителя, меньшего n , то число n — простое и ответом будет само число n.
+ Формат входных данных
+ Вводится натуральное число.
+ Формат выходных данных
+ Выведите ответ на задачу.
+ Sample Input 1:
+ 4
+ Sample Output 1:
+ 2
+ 
+ Sample Input 2:
+ 5
+ Sample Output 2:
+ 5
+
+ */
+
 #include <iostream>
+#include <cmath>
+#include <climits>
 
 using namespace std;
 
+int minDivisor(int n) {
+  
+  for (int i = 2; i < sqrt(n) + 1; i++) {
+    if (n % i == 0) return i;
+  }
+  
+  return n;
+}
+
 int main() {
   
-  cout << "Hello, World!\n";
+  int n;
+  cin >> n;
+  
+  cout << minDivisor(n);
   
   return 0;
 }
--- 1_9_Functions/Task9/main.cpp
+++ 1_9_Functions/Task9/main.cpp
@@ -1,10 +1,52 @@
+/*
+ Дано действительное положительное число a и целоe число n.
+ 
+ Вычислите an. Решение оформите в виде рекурсивной функции power(a, n).
+ 
+ Формат входных данных
+ Вводится действительное положительное число a и целоe число n.
+ Формат выходных данных
+ Выведите ответ на задачу.
+ Sample Input 1:
+ 2
+ 1
+ Sample Output 1:
+ 2
+ 
+ Sample Input 2:
+ 2
+ 2
+ Sample Output 2:
+ 4
+ */
+
 #include <iostream>
+#include <cmath>
+#include <climits>
 
 using namespace std;
 
+double power(double a, int n) {
+  
+  if (n == 0) return 1;
+  if (n == 1) {
+    return a;
+  } else if(n >1 ) {
+    return a*power(a, n-1);
+  } else if (a == -1) {
+    return 1/a;
+  } else {
+    return 1/a*power(a, n+1);
+  }
+}
+
 int main() {
   
-  cout << "Hello, World!\n";
+  double a;
+  int n;
+  cin >> a >> n;
+  
+  cout << power(a, n);
   
   return 0;
 }
