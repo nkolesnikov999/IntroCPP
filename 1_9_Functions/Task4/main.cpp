104b6995953b0f315455aa04c0e5cc438f975164
nk <nkolesnikov@mail.ru>
2016-09-07 12:57:50 +0300

    IntroCPP_1_9_Task6

--- 1_9_Functions/Task4/main.cpp
+++ 1_9_Functions/Task4/main.cpp
@@ -1,10 +1,35 @@
+/*
+ Даны четыре действительных числа: x1, y1, x2, y2. Напишите функцию distance(x1, y1, x2, y2), вычисляющую расстояние между точкой (x1. y1) и (x2, y2). Считайте четыре действительных числа и выведите результат работы этой функции.
+ 
+ Формат входных данных
+ Вводятся четыре действительных числа.
+ Формат выходных данных
+ Выведите ответ на задачу.
+ Sample Input:
+ 0
+ 0
+ 1
+ 1
+ Sample Output:
+ 1.41421
+ */
+
 #include <iostream>
+#include <math.h>
+#include <limits.h>
 
 using namespace std;
 
+double distance(double x1, double y1, double x2, double y2) {
+  return sqrt(pow((x2-x1),2) + pow((y2-y1),2));
+}
+
 int main() {
   
-  cout << "Hello, World!\n";
+  double a, b, c, d;
+  cin >> a >> b >> c >> d;
+  
+  cout << distance(a, b, c, d);
   
   return 0;
 }
