1fee6da816ba6dd4919a886f23f6a50a34c1adfd
nk <nkolesnikov@mail.ru>
2016-09-08 08:27:14 +0300

    IntroCPP_1_10_Task6

--- 1_10_Strings/Task3/main.cpp
+++ 1_10_Strings/Task3/main.cpp
@@ -1,10 +1,43 @@
+/*
+ По введенному символу определите, является ли он цифрой.
+ 
+ Входные данные
+ 
+ Вводится единственый символ.
+ 
+ Выходные данные
+ 
+ Выведите "yes", если символ является цифрой и "no" в противном случае. Обратите внимание, что слова нужно выводить маленькими буквами.
+ 
+ Sample Input 1:
+ 1
+ Sample Output 1:
+ yes
+ 
+ Sample Input 2:
+ a
+ Sample Output 2:
+ no
+ */
+
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+
 int main() {
   
-  cout << "Hello, World!\n";
+  string s;
+  cin >> s;
+  
+  char c = s[0];
+  
+  if (c >= '0' && c <= '9') {
+    cout << "yes";
+  } else {
+    cout << "no";
+  }
   
   return 0;
 }
