c5882e799af01e07747344fd89f74dc44d4567f1
nk <nkolesnikov@mail.ru>
2016-09-08 22:12:50 +0300

    IntroCPP_1_10_Task8

--- 1_10_Strings/Task7/main.cpp
+++ 1_10_Strings/Task7/main.cpp
@@ -1,10 +1,51 @@
+/*
+ По данной строке определите, является ли она палиндромом (то есть, читается одинаково как слева-направо, так и справа-налево).
+ Входные данные
+ 
+ На вход подается 1 строка без пробелов.
+ Выходные данные
+ 
+ Необходимо вывести yes, если строка является палиндромом, и no в противном случае.
+ 
+ Sample Input:
+ kayak
+ Sample Output:
+ yes
+ */
+
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+char downcase(char c) {
+  if (c >= 'a' && c <= 'z') {
+    return c;
+  }
+  if (c >= 'A' && c <= 'Z') {
+    return c - 'A' + 'a';
+  }
+  return NULL;
+}
+
 int main() {
   
-  cout << "Hello, World!\n";
+  string s;
+  cin >> s;
+  
+  bool check = true;
+  long n = s.size();
+  
+  for (int i = 0; i < n/2; i++) {
+    
+    if(downcase(s[i]) != downcase(s[n-1-i])) check = false;
+  }
+  
+  if (check) {
+    cout << "yes";
+  } else {
+    cout << "no";
+  }
   
   return 0;
 }
--- 1_10_Strings/Task8/main.cpp
+++ 1_10_Strings/Task8/main.cpp
@@ -1,10 +1,60 @@
+/*
+ Найдите в данной строке самое длинное слово и выведите его.
+ 
+ Входные данные
+ 
+ Вводится одна строка. Слова в ней отделены одним пробелом.
+ 
+ Выходные данные
+ 
+ Выведите самое длинное слово. Если таких слов несколько, то выводить нужно, которое встречается раньше.
+ 
+ Sample Input:
+ Everyone of us has all we need
+ Sample Output:
+ Everyone
+ */
+
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main() {
   
-  cout << "Hello, World!\n";
+  string s;
+  getline(cin, s);
+  
+  long lengthString = s.length();
+  
+  string word;
+  int max = 0;
+  int start = 0;
+  long position = 0;
+  int length = 0;
+  
+  while (true) {
+    position = s.find(" ", position);
+    if (position == -1) {
+      length = (int)lengthString - start;
+      if (length > max) {
+        max = length;
+        word = s.substr(start, length);
+      }
+      break;
+    } else {
+      length = (int)position - start;
+      if (length > max) {
+        max = length;
+        word = s.substr(start, length);
+      }
+      position++;
+      start = (int)position;
+    }
+  }
+  
+  cout << word;
+  
   
   return 0;
 }
