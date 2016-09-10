d2aa525577401fa19db052e8bf76e828894e8210
nk <nkolesnikov@mail.ru>
2016-09-10 19:15:26 +0300

    IntroCPP_1_9_Task5

--- 1_12_AlgoritmsSTL/Task3/main.cpp
+++ 1_12_AlgoritmsSTL/Task3/main.cpp
@@ -1,10 +1,45 @@
+/*
+ Отсортируйте массив.
+ 
+ Входные данные
+ 
+ Первая строка входных данных содержит количество элементов в массиве N ≤ 105. Далее идет N целых чисел, не превосходящих по абсолютной величине 109.
+ 
+ Выходные данные
+ 
+ Выведите эти числа в порядке неубывания.
+ Sample Input:
+ 5
+ 5 4 3 2 1
+ Sample Output:
+ 1 2 3 4 5
+ */
+
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int main() {
   
-  cout << "Hello, World!\n";
+  int n;
+  cin >> n;
+  
+  vector<int> intVec;
+  
+  for (int i = 0; i < n; i++) {
+    int temp;
+    cin >> temp;
+    intVec.push_back(temp);
+  }
+  
+  sort(intVec.begin(), intVec.end());
+  for (auto now : intVec) {
+    
+    cout << now << " ";
+  }
+  
   
   return 0;
 }
--- 1_12_AlgoritmsSTL/Task5/main.cpp
+++ 1_12_AlgoritmsSTL/Task5/main.cpp
@@ -1,10 +1,68 @@
+/*
+ Во время проведения олимпиады каждый из участников получил свой идентификационный номер – натуральное число. Необходимо отсортировать список участников олимпиады по количеству набранных ими баллов.
+ 
+ Входные данные
+ 
+ На первой строке дано число N (1 ≤ N ≤ 1000) – количество участников. На каждой следующей строке даны идентификационный номер и набранное число баллов соответствующего участника. Все числа во входном файле не превышают 105.
+ 
+ Выходные данные
+ 
+ В выходной файл выведите исходный список в порядке убывания баллов. Если у некоторых участников одинаковые баллы, то их между собой нужно упорядочить в порядке возрастания идентификационного номера.
+ Sample Input 1:
+ 3
+ 101 80
+ 305 90
+ 200 14
+ Sample Output 1:
+ 305 90
+ 101 80
+ 200 14
+
+ */
+
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
+struct man {
+  int id;
+  int value;
+};
+
+bool cmp(man a, man b) {
+  if (a.value != b.value) {
+    return a.value > b.value;
+  } else {
+    return a.id < b.id;
+  }
+}
+
 int main() {
   
-  cout << "Hello, World!\n";
+  int n;
+  cin >> n;
+  
+  vector<man> intVec(n);
+  
+  for (int i = 0; i < n; i++) {
+    int temp;
+    int v_temp;
+    cin >> temp >> v_temp;
+    man struct_temp; // временная структура
+    struct_temp.id = temp;
+    struct_temp.value = v_temp;
+    intVec[i] = struct_temp;
+  }
+  
+  sort(intVec.begin(), intVec.end(), cmp);
+  
+  for (auto now : intVec) {
+    cout << now.id << " " << now.value << "\n";
+  }
+  
+  
   
   return 0;
 }
