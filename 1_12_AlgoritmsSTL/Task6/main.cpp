01c6a6a0e489ad0ed563a58c36be6350b06e67b4
nk <nkolesnikov@mail.ru>
2016-09-10 20:20:06 +0300

    IntroCPP_1_12_Full

--- 1_12_AlgoritmsSTL/Task6/main.cpp
+++ 1_12_AlgoritmsSTL/Task6/main.cpp
@@ -1,10 +1,60 @@
-#include <iostream>
+/*
+ Выведите все исходные точки в порядке возрастания их расстояний от начала координат.
+ 
+ Входные данные
+ 
+ Программа получает на вход набор точек на плоскости. Сначала задано количество точек n, затем идет последовательность из n строк, каждая из которых содержит два числа: координаты точки. Величина n не превосходит 100, все исходные координаты – целые числа, не превосходящие 103.
+ 
+ Выходные данные
+ 
+ Необходимо вывести все исходные точки в порядке возрастания их расстояний от начала координат.
+ 
+ Sample Input:
+ 2
+ 1 2
+ 2 3
+ Sample Output:
+ 1 2
+ 2 3
+ */
+
+include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
+struct man {
+  int x;
+  int y;
+};
+
+bool cmp(man a, man b) {
+  return a.x*a.x + a.y*a.y < b.x*b.x + b.y*b.y;
+}
+
 int main() {
   
-  cout << "Hello, World!\n";
+  int n;
+  cin >> n;
+  
+  vector<man> intVec(n);
+  
+  for (int i = 0; i < n; i++) {
+    int x_temp;
+    int y_temp;
+    cin >> x_temp >> y_temp;
+    man struct_temp; // временная структура
+    struct_temp.x = x_temp;
+    struct_temp.y = y_temp;
+    intVec[i] = struct_temp;
+  }
+  
+  sort(intVec.begin(), intVec.end(), cmp);
+  
+  for (auto now : intVec) {
+    cout << now.x << " " << now.y << "\n";
+  }
   
   return 0;
 }
--- 1_12_AlgoritmsSTL/Task7/main.cpp
+++ 1_12_AlgoritmsSTL/Task7/main.cpp
@@ -1,10 +1,68 @@
+/*
+ Выведите фамилии и имена учащихся в порядке убывания их среднего балла.
+ Входные данные
+ 
+ Заданы сначала количество учащихся n, затем n строк, каждая из которых содержит фамилию, имя и три числа (оценки по трем предметам: математике, физике, информатике). Данные в строке разделены одним пробелом. Оценки принимают значение от 1 до 5.
+ 
+ Выходные данные
+ 
+ Необходимо вывести пары фамилия-имя по одной на строке, разделяя фамилию и имя одним пробелом. Выводить оценки не нужно. Если несколько учащихся имеют одинаковые средние баллы, то их нужно выводить в порядке, заданном во входных данных.
+ 
+ Sample Input:
+ 3
+ Markov Valeriy 5 5 5
+ Sergey Petrov 1 1 1
+ Petrov Petr 3 3 3
+ Sample Output:
+ Markov Valeriy
+ Petrov Petr
+ Sergey Petrov
+ */
+
+#include <string>
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
+struct man {
+  string lname;
+  string fname;
+  double math;
+  double phis;
+  double info;
+};
+
+bool cmp(man a, man b) {
+  return (a.math + a.phis + a.info)/3 > (b.math + b.phis + b.info)/3;
+}
+
 int main() {
   
-  cout << "Hello, World!\n";
+  int n;
+  cin >> n;
+  
+  vector<man> intVec(n);
+  
+  for (int i = 0; i < n; i++) {
+    string l_temp, n_temp;
+    double m_temp, p_temp, i_temp;
+    cin >> l_temp >> n_temp >> m_temp >> p_temp >> i_temp;
+    man struct_temp; // временная структура
+    struct_temp.lname = l_temp;
+    struct_temp.fname = n_temp;
+    struct_temp.math = m_temp;
+    struct_temp.phis = p_temp;
+    struct_temp.info = i_temp;
+    intVec[i] = struct_temp;
+  }
+  
+  stable_sort(intVec.begin(), intVec.end(), cmp);
+  
+  for (auto now : intVec) {
+    cout << now.lname << " " << now.fname << "\n";
+  }
   
   return 0;
 }
